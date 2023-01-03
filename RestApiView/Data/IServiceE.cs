e9f6ae7a05ba21c6a8e0e758712680b1377e0ff5
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2023-01-03 16:45:47 +0100

    tt

---
+++ RestApiView/Data/IServiceE.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiView.Data
+{
+    public interface IServiceE
+    {
+
+        List<Entreprise> Entreprises { get; set; }
+
+        Task<List<Entreprise>> GetEntreprise();
+    }
+}
---
+++ RestApiView/Data/ServiceE.cs
@@ -0,0 +1,90 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace RestApiView.Data
+{
+    public class ServiceE : ComponentBase
+    {
+
+        [Inject]
+        HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected List<Entreprise> entrepriseList = new();
+
+       
+        protected override async Task OnInitializedAsync()
+        {
+            await GetEntreprise();
+        }
+
+        protected async Task GetEntreprise()
+        {
+            entrepriseList = await Http.GetFromJsonAsync<List<Entreprise>>("api/Entreprise");
+            
+        }
+
+       
+
+        protected async Task DeleteEntreprise(int EntreID)
+        {
+            await Http.DeleteAsync("api/Entreprise/delete/" + EntreID);
+            await GetEntreprise();
+        }
+
+
+     
+
+        
+
+        [Parameter]
+        public int EntreID { get; set; }
+
+        protected string Title = "Add";
+        public Entreprise entreprise = new();
+      
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (EntreID != 0)
+            {
+                Title = "Edit";
+                entreprise = await Http.GetFromJsonAsync<Entreprise>("api/Entreprise/" + EntreID);
+            }
+        }
+
+
+        protected async Task SaveEntreprise()
+        {
+            if (entreprise.EntrepriseId != 0)
+            {
+                await Http.PutAsJsonAsync("api/Entreprise/update", entreprise);
+            }
+            else
+            {
+                await Http.PostAsJsonAsync("api/Entreprise/create", entreprise);
+            }
+            Cancel();
+        }
+
+        protected void DeleteConfirm(int entreID)
+        {
+            entreprise = entrepriseList.FirstOrDefault(x => x.EntrepriseId == entreID);
+          //  Console.WriteLine(entreprise);
+        }
+
+        public void Cancel()
+        {
+            NavigationManager.NavigateTo("/entreprise");
+        }
+
+    }
+}
